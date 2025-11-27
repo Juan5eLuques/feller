20d77b22706ac2de372e3bc567213be6918019b2
Juan Sebastian Luques <luquesjuanse.10@gmail.com>
2025-11-27 02:41:37 -0300

    fix deploy

--- next.config.js
+++ next.config.js
@@ -2,12 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'fellerbackend-production.up.railway.app'],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '5000',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'fellerbackend-production.up.railway.app',
         pathname: '/images/**',
       },
     ],
